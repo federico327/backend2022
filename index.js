6e5a0145dd349608ffe1382d000d611af0b73b77
Federico Romero <federicoromero150@gmail.com>
2022-08-24 19:55:57 -0300

    primer entrega2

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-
-
 class Usuario {
     constructor (nombre, apellido, libros, mascotas){
         this.nombre = nombre ;
         this.apellido = apellido ;
-        this.libros =  [{titulo:"titulo1", autor:"autor1"}, {titulo:"titulo2", autor:"autor2"}];
-        this.mascotas =  ["Aica" , "Draco" , "Beba" , "Piqui"];
-    }     
-    getFullName (){
-        return `${ this.nombre}+ ${ this.apellido}`
+        this.libros = [];
+        this.mascotas = [];
+
+    }  
+    
+    getFullName(){ 
+        return (`nombre completo: ${this.nombre}  ${this.apellido}`)
+
+    };
+
+    addMascotas(mascota){
+        this.mascotas.push(mascota)
+        
+    };
+
+    countMascotas(){
+        return (this.mascotas.length)
     }
-}
+
+    addBook(titulo, autor){
+        this.libros.push([{titulo, autor}])
+    }
+
+    getBookName() {
+
+        return this.libros.map(object =>{
+            return (object.nombre);
+        }); 
+
+        //return this.libros.filter(autor => autor.nombre === autor)
+
+    } 
+
+    
+};
 
 
-getFullName ()
+const persona = new Usuario ("Fede", "Romero")
+
+console.log (persona.getFullName())
+
+persona.addMascotas("Draco")
+persona.addMascotas("Aica")
+persona.addMascotas("Beba")
+persona.addMascotas("Piqui")
+
+console.log (persona.mascotas)
+
+console.log (persona.countMascotas())
+
+persona.addBook("The Lord of the Rings" , "J. R. R. Tolkien")
+persona.addBook("El poder de los hábitos" , "Charles Duhigg")
+
+console.log(persona.libros)
+
+
+console.log(persona.getBookNames())
