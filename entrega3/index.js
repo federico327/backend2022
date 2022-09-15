32bcd6df24199c2e5a4cf7930e6b90ca3aef17a7
Federico Romero <federicoromero150@gmail.com>
2022-09-14 22:21:18 -0300

    entrega4

--- entrega3/index.js
+++ entrega3/index.js
@@ -1,19 +1,76 @@
-const express = require ('express');
-const {products} = require('./data')
-
+const express = require ('express')
+const {products} = require("./data.js")
 
 const PORT = process.env.PORT || 8080;
-//createserver
+
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send('esta es la pagina de inicio')
-});
 
-app.get('/productos',(req,res) =>{
+
+app.get('/', (req, res) => {
+    res.send('Esta es la pagina de inicio')
+})
+
+
+
+app.get('/api/productos', (req, res) => {
     let response = products
-    res.json(response)
-});
+    
+    
+    if(req.query.title){
+        const newProducts = products.filter(product => {
+            return product.title == req.query.title;
+        })
+
+        response = newProducts
+    }
+
+    if(req.query.maxPrice){
+        const newProducts = products.filter(product => {
+            return product.price <= req.query.maxPrice;
+        })
+
+        response = newProducts
+    }
+
+    res.json(response);
+})
+
+
+
+app.post('/api/productos', (req, res) => {
+    const ids = products.map(object => {
+        return object.id;
+    });
+    const max = Math.max(...ids);
+    req.query.id = max + 1
+    req.query.price = Number(req.query.price)
+    
+    products.push(req.query)
+    res.json(products)
+})
+
+
+
+app.delete('/api/productos/:id', (req, res) => {
+    const newProducts = products.filter(product => {
+        return product.id != req.params.id;
+    })
+    res.json(newProducts);
+})
+
+
+
+app.put('/api/productos/:id', (req, res) => {
+    const product = products.filter(product => {
+        req.query.title ? product.title = req.query.title : null
+        req.query.price ? product.price = req.query.price : null
+        return product.id == req.params.id;
+    })
+
+    res.json(product);
+})
+
 
 
 app.get('/productoRandom', (req, res) => {
@@ -24,14 +81,13 @@ app.get('/productoRandom', (req, res) => {
 })
 
 
-
 const connectedServer = app.listen(PORT, () => {
     console.log (`Server is up and running on port ${PORT}`)
 })
 
 connectedServer.on('error', (error) => {
     console.log(error.message); 
-});
+})
 
 
 
